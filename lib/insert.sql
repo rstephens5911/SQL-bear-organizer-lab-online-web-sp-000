6720b223f0486a3bae1a931b939aff8ce5a55161
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-03 00:48:55 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 2, "male", "dark brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 4, "male", "orange", "comatose", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 5, "female", "yellow", "flat", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 7, "male", "black", "blackout drunk", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 5, "male",  "white", "co-dependant", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 15, "male", "tan", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 12, "female", "brown", "bear-like", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, NULL, "male", "pink", "slightly upset", 0);
